fe44fe71eea4e3736f5b6c347aec8b85939da77f
ANIRUDH K <147629094+Abhi-anirudh@users.noreply.github.com>
2025-12-09 05:28:55 +0000

    day2

---
+++ day2.js
@@ -0,0 +1,13 @@
+let score ="33AABB"
+let a="abc"
+let b=""
+let c=null
+let d;
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+console.log(typeof(score), typeof (score))
+let v = Number(score)
+console.log(typeof(v), typeof (v),v)
